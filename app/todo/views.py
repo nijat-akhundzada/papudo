d058b224db13f48122edc4b943c65182ba5207c7
NihaMursalli <nihatmursalli30@gmail.com>
2025-06-20 17:14:56 +0400

    Updated todo view and added an update option

--- app/todo/views.py
+++ app/todo/views.py
@@ -50,3 +50,26 @@ def get_todos(request):
         e = str(e)
         error = {'error': e}
         request.wfile.write(json.dumps(error).encode())
+
+def update_todos(request):
+    content_length = int(request.headers.get('Content-Length', 0))
+    body = request.rfile.read(content_length)
+    try:
+        todo_data = json.loads(body.decode('utf-8'))
+        update(**todo_data)
+        response = {'message': 'Successfully updated!'}
+        request.send_response(200)
+        request.send_header("Content-Type", "application/json")
+        request.end_headers()
+        request.wfile.write(json.dumps(response).encode())
+    except json.JSONDecodeError:
+        request.send_response(400)
+        request.end_headers()
+        request.wfile.write(b'Invalid JSON')
+    except IntegrityError as e:
+        request.send_response(400)
+        request.end_headers()
+        e = str(e)
+        error = {'error': e}
+        request.wfile.write(json.dumps(error).encode())
+
